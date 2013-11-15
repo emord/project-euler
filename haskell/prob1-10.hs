01c2f9b90c710365d4a385e763f9fcd97f7ad4d1
Jonathan Emord <jonathan.emord@gmail.com>
2013-11-14 23:24:58 -0500

    More haskell

--- haskell/prob1-10.hs
+++ haskell/prob1-10.hs
@@ -1,3 +1,6 @@
+import Data.List
+import qualified Math.NumberTheory.Primes.Sieve as PS
+
 mod3or5 :: Integer -> Integer
 mod3or5 x = if mod x 3 == 0 || mod x 5 == 0
             then x
@@ -30,4 +33,28 @@ isPalindrome x = (show x) == (reverse $ show x)
 prob4 :: Integer
 prob4 = maximum [z | x <- [100..999], y <- [x..999], let z = x * y, isPalindrome z]
 
+prob5 :: Integer
 prob5 = foldl1 lcm [1..20]
+
+square :: Integer -> Integer
+square = (^ 2)
+
+prob6 :: Integer
+prob6 = (square $ sum [1..100]) - (sum $ map square [1..100])
+
+some_remainder :: Integer -> Integer -> Bool
+some_remainder x y = (y `mod` x) /= 0
+
+primes :: [Integer] -> [Integer]
+primes (x:xs) = x : primes (filter (some_remainder x) xs)
+
+prob7 :: Integer
+prob7 = primes [2..] !! 10000
+
+hypot a b = sqrt $ a * a + b * b
+
+tripletSum a b = a + b + hypot a b
+
+prob9 = head [a * b * hypot a b | a <- [1..1000], b <- [1..a], tripletSum a b == 1000]
+
+prob10 = sum $ takeWhile (< 2 * 10 ^ 6) PS.primes
